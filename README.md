7347ad65e469eb100158b6ac26d0643152fa9d59
Serious-Fin <frystorm4@gmail.com>
2025-04-26 16:57:00 +0300

    minor improvements

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # portfolio
 
 This is my portfolio website created with HTML, CSS and JavaScript. Additionally, [fullPage.js](https://alvarotrigo.com/fullPage/) plug-in was used.
+
+TODO:
+
+- add skills
+- add navbar, but only after animation plays
+- Add some random rotating shapes throughout the webpage
--- scripts/loadFullpage.js
+++ scripts/loadFullpage.js
@@ -1,19 +1,16 @@
 new fullpage("#fullpage", {
-    autoScrolling: true,
-    scrollHorizontally: true,
-    onLeave: function (origin, destination, direction) {
-        console.log(
-            `origin.index=${origin.index} destination.index=${destination.index} direction=${direction}`
-        );
-        if (origin.index === 0 && direction === "down" && !hasAnimationPlayed) {
-            playOpenAnimation();
-            return false;
-        }
-        if (origin.index === 1 && direction === "up" && hasAnimationPlayed) {
-            playCloseAnimation();
-            return false;
-        }
-    },
+  autoScrolling: true,
+  scrollHorizontally: true,
+  onLeave: function (origin, destination, direction) {
+    if (origin.index === 0 && direction === "down" && !hasAnimationPlayed) {
+      playOpenAnimation();
+      return false;
+    }
+    if (origin.index === 1 && direction === "up" && hasAnimationPlayed) {
+      playCloseAnimation();
+      return false;
+    }
+  },
 });
 
 const video = document.getElementById("intro-video");
@@ -28,73 +25,68 @@ let isOpening = true;
 let hasAnimationPlayed = false;
 
 video.onloadeddata = () => {
-    duration = video.duration;
-    midwayPoint = duration / 2;
+  duration = video.duration;
+  midwayPoint = duration / 2;
 };
 
 function playOpenAnimation() {
-    isOpening = true;
-    hideScrollTip();
-    if (video.currentTime === midwayPoint) {
-        return;
-    }
-    video.currentTime =
-        video.currentTime > midwayPoint
-            ? duration - video.currentTime
-            : video.currentTime;
-    video.play();
+  isOpening = true;
+  hideScrollTip();
+  if (video.currentTime === midwayPoint) {
+    return;
+  }
+  video.currentTime = video.currentTime > midwayPoint ? duration - video.currentTime : video.currentTime;
+  video.play();
 }
 
 function playCloseAnimation() {
-    isOpening = false;
-    hasAnimationPlayed = false;
-    if (video.currentTime === 0 || video.currentTime === duration) {
-        return;
-    }
-    video.currentTime =
-        video.currentTime < midwayPoint
-            ? duration - video.currentTime
-            : video.currentTime;
-    video.play();
+  isOpening = false;
+  hasAnimationPlayed = false;
+  if (video.currentTime === 0 || video.currentTime === duration) {
+    return;
+  }
+  video.currentTime = video.currentTime < midwayPoint ? duration - video.currentTime : video.currentTime;
+  video.play();
 }
 
 video.addEventListener("timeupdate", () => {
-    if (isOpening && video.currentTime > midwayPoint) {
-        hasAnimationPlayed = true;
-        video.currentTime = midwayPoint;
-        video.pause();
-        showAboutText();
-    }
+  if (isOpening && video.currentTime > midwayPoint) {
+    hasAnimationPlayed = true;
+    video.currentTime = midwayPoint;
+    video.pause();
+    showAboutText();
+    fullpage_api.moveSectionDown();
+  }
 
-    if (!isOpening || video.currentTime !== midwayPoint) {
-        hideAboutText();
-    }
+  if (!isOpening || video.currentTime !== midwayPoint) {
+    hideAboutText();
+  }
 
-    if (video.currentTime === 0 || video.currentTime === duration) {
-        showScrollTip();
-    }
+  if (video.currentTime === 0 || video.currentTime === duration) {
+    showScrollTip();
+  }
 });
 
 function showAboutText() {
-    for (let i = 0; i < text.length; i++) {
-        text[i].classList.remove("invisible");
-    }
+  for (let i = 0; i < text.length; i++) {
+    text[i].classList.remove("invisible");
+  }
 }
 
 function hideAboutText() {
-    for (let i = text.length - 1; i >= 0; i--) {
-        text[i].classList.add("invisible");
-    }
+  for (let i = text.length - 1; i >= 0; i--) {
+    text[i].classList.add("invisible");
+  }
 }
 
 function showScrollTip() {
-    for (let i = 0; i < scrollTips.length; i++) {
-        scrollTips[i].classList.remove("invisible");
-    }
+  for (let i = 0; i < scrollTips.length; i++) {
+    scrollTips[i].classList.remove("invisible");
+  }
 }
 
 function hideScrollTip() {
-    for (let i = scrollTips.length - 1; i >= 0; i--) {
-        scrollTips[i].classList.add("invisible");
-    }
+  for (let i = scrollTips.length - 1; i >= 0; i--) {
+    scrollTips[i].classList.add("invisible");
+  }
 }
